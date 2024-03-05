602e04bb9d6bb674530336f128a20bebc6470052
ksangwan428 <128834294+ksangwan428@users.noreply.github.com>
2024-03-05 16:11:23 +0530

    TrafficConsumer.cpp

--- Module 2/M2.T3D - Traffic Control Simulator/TrafficConsumer.cpp
+++ Module 2/M2.T3D - Traffic Control Simulator/TrafficConsumer.cpp
@@ -1,6 +1,3 @@
-// kartik  sangwan 
-//2210994801
-
 #include <iostream>
 #include <fstream>
 #include <ctime>
@@ -8,68 +5,88 @@
 using namespace std;
 
 #define NUM_LIGHTS 12
-#define MEASUREMENTS 13     // 12 per hour plus held average value
+#define MEASUREMENTS 13 // 12 per hour plus held average value
 
 // Function to print details of traffic measurements
-void printDetails(int array[NUM_LIGHTS][MEASUREMENTS]){
-    for(int i = 1; i <= NUM_LIGHTS ; i++){
-        cout << to_string(i) + ":\t";
-        for (int j = 0 ; j < sizeof(array[i])/4 ; j++){
-            cout << to_string(array[i-1][j]) + ";";
+void printDetails(int array[NUM_LIGHTS][MEASUREMENTS])
+{
+    for (int i = 0; i < NUM_LIGHTS; i++)
+    {
+        cout << to_string(i + 1) + ":\t";
+        for (int j = 0; j < MEASUREMENTS; j++)
+        {
+            cout << to_string(array[i][j]) + ";";
         }
         cout << endl;
     }
 }
 
 // Function to calculate average traffic volume for each traffic light
-void caluclateTrafficVolume(int array[NUM_LIGHTS][MEASUREMENTS]){
+void calculateTrafficVolume(int array[NUM_LIGHTS][MEASUREMENTS])
+{
     cout << "Calculating...\t";
-    for (int i = 0 ; i < NUM_LIGHTS ; i++){
+    for (int i = 0; i < NUM_LIGHTS; i++)
+    {
         array[i][0] = 0;
-        for (int j = 1 ; j < MEASUREMENTS ; j++){
+        for (int j = 1; j < MEASUREMENTS; j++)
+        {
             array[i][0] += array[i][j];
         }
-        array[i][0] = array[i][0] / (MEASUREMENTS-1); // Integer division for simplicity
+        array[i][0] = array[i][0] / (MEASUREMENTS - 1); // Integer division for simplicity
     }
     cout << "Done" << endl;
 }
 
-int main(){
-
+int main()
+{
     int Array[NUM_LIGHTS][MEASUREMENTS];
-    int arraySize = sizeof(Array[1])/4;      // 4 bits
 
-    long rawTime;
-    int lightNum;
-    int trafficVolume;
-
+    // Open log file for reading
     fstream log("log.txt", ios::in);
-    if(!log.is_open()){
+    if (!log.is_open())
+    {
         cout << "Error: Log file not found" << endl;
-    } else {
+    }
+    else
+    {
         cout << "Retrieving initial data from log.txt...\t";
-        for(int i = 1 ; i <= NUM_LIGHTS ; i++){
-            for (int j = 1 ; j < arraySize ; j++){
-                Array[i-1][j] = 0;
+
+        // Initialize the array with zeros
+        for (int i = 0; i < NUM_LIGHTS; i++)
+        {
+            for (int j = 0; j < MEASUREMENTS; j++)
+            {
+                Array[i][j] = 0;
             }
         }
 
-        log >> rawTime >> lightNum >> trafficVolume;
+        // Read data from the log file
+        long rawTime;
+        int lightNum;
+        int trafficVolume;
 
-        for(int k = 0 ; k < 12 ; k++){
-            for(int i = 0 ; i < NUM_LIGHTS ; i++){
+        for (int k = 0; k < MEASUREMENTS; k++)
+        {
+            for (int i = 0; i < NUM_LIGHTS; i++)
+            {
                 log >> rawTime >> lightNum >> trafficVolume;
 
-                for(int j = sizeof(Array[0]) / sizeof(Array[0][0]) - 1 ; j > 0; j--){
-                    Array[lightNum-1][j] = Array[lightNum-1][j-1];
+                // Shift the values in the array
+                for (int j = MEASUREMENTS - 1; j > 0; j--)
+                {
+                    Array[lightNum - 1][j] = Array[lightNum - 1][j - 1];
                 }
-                Array[lightNum-1][1] = trafficVolume;
+                // Store the new traffic volume
+                Array[lightNum - 1][0] = trafficVolume;
             }
         }
+
         log.close();
         cout << "Complete" << endl;
     }
 
-    caluclateTrafficVolume(Array);
-    // printDetails(Array);
+    calculateTrafficVolume(Array);
+    printDetails(Array);
+
+    return 0;
 }
