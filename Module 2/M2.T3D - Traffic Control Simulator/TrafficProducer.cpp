e47edeffe0c3df9dbbb344d2ecbffb14034420f8
ksangwan428 <128834294+ksangwan428@users.noreply.github.com>
2024-03-05 15:55:01 +0530

    Create TrafficProducer.cpp

---
+++ Module 2/M2.T3D - Traffic Control Simulator/TrafficProducer.cpp
@@ -0,0 +1,39 @@
+```cpp
+#include <iostream>
+#include <random>
+#include <ctime>
+#include <fstream>
+#include <time.h>
+
+using namespace std;
+
+#define NUM_LIGHTS 12       // Number of traffic lights
+
+int main(){
+
+    string output;
+    
+    time_t rawTime;
+    time(&rawTime);
+
+    srand (time(NULL));
+
+    for (int i = 1 ; i <= NUM_LIGHTS ; i++){
+        
+        output += to_string(rawTime) + " ";                  // Log entry timestamp
+        output += to_string(i) + " ";                       // Light number
+        int cars = rand() % ((10 - 1) + 1) + 1;
+        output += to_string(cars) + " \n";                   // Number of cars (random)
+    }
+
+    fstream log("log.txt", ios::app);                       // Append to log file
+    if(!log.is_open()){
+        cout << "Error: Log file not found" << endl;
+    } else {
+        log << output;
+        cout << output;
+        cout << "log.txt has been updated at " << ctime(&rawTime) << endl;
+    }
+    return 0;
+}
+```
